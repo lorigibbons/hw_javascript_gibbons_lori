59d0064d88d090ba9a5e211557e2c7abe0453fb0
Lori Gibbons <lori.j.gibbons@gmail.com>
2017-06-04 17:12:20 -0400

    Added presidents
    Added presidents objects and then made array to display the 5 objects

--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
 var iceCream = ['Pineapple', 'Cookies and Cream', 'Cookie Dough'];
 
 iceCream.push('Coffee');
+
+var presidents1 = {
+    firstName: 'Donald',
+    lastName: 'Trumpf',
+    termLength: 4,
+    party: 'Republican',
+    yearsOfPresidency: 0
+};
+var presidents2 = {
+    firstName: 'Barak',
+    lastName: 'Obama',
+    termLength: 4,
+    party: 'Democrat',
+    yearsOfPresidency: 8
+};
+var presidents3 = {
+    firstName: 'George W.',
+    lastName: 'Bush',
+    termLength: 4,
+    party: 'Republican',
+    yearsOfPresidency: 8
+};
+var presidents4 = {
+    firstName: 'Bill',
+    lastName: 'Clinton',
+    termLength: 4,
+    party: 'Democrat',
+    yearsOfPresidency: 4
+};
+var presidents5 = {
+    firstName: 'George',
+    lastName: 'Bush Sr.',
+    termLength: 4,
+    party: 'Republican',
+    yearsOfPresidency: 8
+};
+var presidents = [presidents1, presidents2, presidents3, presidents4, presidents5];
